bb0b610ed101c8268514f26e6125c744a3698da9
nimo247 <n14082005@gmail.com>
2025-06-27 18:33:31 +0000

    update

--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@ from typing import Optional
 from datetime import datetime
 
 class RegisterModel(BaseModel):
-    username: str = Field(..., min_length=3, max_length=100)
+    username: str = Field(..., min_length=3, max_length=10)
     password: str = Field(..., min_length=6)
 
 class LoginModel(BaseModel):
@@ -17,17 +17,18 @@ class UserOut(BaseModel):
 class TaskCreate(BaseModel):
     title: str
     description: Optional[str] = None
-    status: bool = False
+    status:bool = False 
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    status: bool = False
+    status: bool = False 
+
+from typing import Optional
 
 class TaskOut(BaseModel):
     id: str
     title: str
-    description: Optional[str] = None
-    status: bool
+    description: str
     created_at: datetime
-
+    status: bool=False 
